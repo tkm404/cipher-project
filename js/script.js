0be81a1420776f969fc0174b25adc717ba1e6596
tmcdowellddhs@yahoo.com <tmcdowellddhs@yahoo.com>
2023-03-20 15:42:13 -0700

    add function to script.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+function scramble(textToScramble) {
+  return textToScramble;
+}
+
 const textToScramble = prompt("Enter thine text for scramblage... if thou dare!");
 
 window.alert(textToScramble) ; 
