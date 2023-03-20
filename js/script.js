12832755ac6fa77a6860f67af8633af14e672f2b
tmcdowellddhs@yahoo.com <tmcdowellddhs@yahoo.com>
2023-03-20 16:38:24 -0700

    add switcheroo function.

--- js/script.js
+++ js/script.js
@@ -6,3 +6,11 @@ const textToScramble = prompt("Enter thine text for scramblage... if thou dare!"
 
 window.alert(scramble(textToScramble)) ; 
 
+function switcheroo(_textToScramble){
+return (lastLetter.toUpperCase() + firstLetter.toUpperCase());
+} 
+
+const firstLetter = textToScramble.charAt(0) ;
+const lastLetter = textToScramble.charAt(3) ;
+
+window.alert(switcheroo(textToScramble));
